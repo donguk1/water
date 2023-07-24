85713ceaaf862d5c096147f7cbfa518869a8b8ee
Leedonguk <donguk0105@naver.com>
2023-07-24 20:22:58 +0900

    명칭 수정 및 자잘한거 수정

--- src/main/java/kopo/poly/waterApplication.java
+++ src/main/java/kopo/poly/waterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class k_waterApplication {
+public class waterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(k_waterApplication.class, args);
+        SpringApplication.run(waterApplication.class, args);
     }
 
 }
