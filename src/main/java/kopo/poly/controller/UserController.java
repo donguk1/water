5974214a2d1149989e533a09052f37d50c0862f3
leesihyeon <pubu0880@naver.com>
2023-08-06 01:31:07 +0900

    카카오 로그인에 필요한 컨트롤러 추가 (리턴값 아직 구현 안함) 현재 데이터 가져오는 것만 확인

--- src/main/java/kopo/poly/controller/UserController.java
+++ src/main/java/kopo/poly/controller/UserController.java
@@ -1,20 +1,32 @@
 package kopo.poly.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kopo.poly.dto.KakaoDTO;
+import kopo.poly.dto.KakaoUserInfoDTO;
 import kopo.poly.dto.UserDTO;
 import kopo.poly.service.IUserService;
 import kopo.poly.util.CmmUtil;
 import kopo.poly.util.EncryptUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
@@ -267,7 +279,94 @@ public class UserController {
 
     /*  패스워드 찾기(현재 페이지 존재하지 않음)  */
 
+    /* 카카오 로그인 엑세스 토큰 받기*/
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code, HttpServletRequest request ) { //Data를 리턴해주는 컨트롤러 함수
 
+        // POST 방식으로 key=value 데이터를 요청 (카카오 쪽으로)
+
+        // http 요청을 편하게 할 수 있다. Retrofit2, OkHttp, RestTemplate 라이브러리가 있다.
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "dd3d60b3f3cd9de73af963a2feb15b30");
+        params.add("redirect_url", "http://localhost:11000/auth/kakao/callback");
+        params.add("code", code);
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+
+        // Http 요청하기 - POST 방식으로 - 그리고 response 변수의 응답 받음.
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // 객체에 담아볼 것이다. Gson, Json Simple, ObjectMapper 라이브러리가 있다.
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        KakaoDTO kakaoDTO = null;
+
+        try {
+            kakaoDTO = objectMapper.readValue(response.getBody(),KakaoDTO.class);
+        } catch (JsonMappingException e) {
+            log.info(e.toString());
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            log.info(e.toString());
+            e.printStackTrace();
+        }
+
+        log.info(this.getClass().getName() + ".카카오 엑세스 토큰 : " + kakaoDTO.getAccess_token());
+
+        // http 요청을 편하게 할 수 있다. Retrofit2, OkHttp, RestTemplate 라이브러리가 있다.
+        RestTemplate rt2 = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " + kakaoDTO.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>>kakaoProfileRequest = new HttpEntity<>(headers2);
+
+        // Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음.
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class
+        );
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoUserInfoDTO kakaoUserInfoDTO = null;
+        try {
+            kakaoUserInfoDTO = objectMapper2.readValue(response2.getBody(),KakaoUserInfoDTO.class);
+        } catch (JsonMappingException e) {
+            log.info(e.toString());
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            log.info(e.toString());
+            e.printStackTrace();
+        }
+
+        log.info(this.getClass().getName() + ".카카오 아이디(번호) : " + kakaoUserInfoDTO.getId());
+        log.info(this.getClass().getName() + ".카카오 이메일 : " + kakaoUserInfoDTO.getKakao_account().getEmail());
+        log.info(this.getClass().getName() + ".카카오 생일 : " + kakaoUserInfoDTO.getKakao_account().getBirthday());
+        log.info(this.getClass().getName() + ".카카오 성별 : " + kakaoUserInfoDTO.getKakao_account().getGender());
+        log.info(this.getClass().getName() + ".카카오 닉네임 : " + kakaoUserInfoDTO.getProperties().getNickname());
+
+        return response2.getBody();
+    }
 
     /*  마이페이지로 이동 = "/user/mypage"  */
     @GetMapping(value = "/user/mypage")
