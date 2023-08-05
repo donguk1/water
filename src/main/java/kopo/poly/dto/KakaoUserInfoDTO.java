6ca452256231bd2e6e64115af713b17b2884fd05
leesihyeon <pubu0880@naver.com>
2023-08-06 01:32:17 +0900

    데이터 가져올 수 있게 수정 (전체수정돼서 비교하고 받으세요)

--- src/main/java/kopo/poly/dto/KakaoUserInfoDTO.java
+++ src/main/java/kopo/poly/dto/KakaoUserInfoDTO.java
@@ -1,42 +1,50 @@
 package kopo.poly.dto;
 
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Properties;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Getter
 @Setter
 @Data
 public class KakaoUserInfoDTO {
-
-    private Long id;
-
+    private String id;
     private String connected_at;
-
     private Properties properties;
-
     private KakaoAccount kakao_account;
-
+    private Map<String, Object> additional_properties = new LinkedHashMap<>();
 
     @Data
-    public class properties {
-
+    public static class Properties {
         private String nickname;
-
-        private String profile_image;
-
-        private String thumbnail_image;
-
+        private Map<String, Object> additional_properties = new LinkedHashMap<>();
     }
 
     @Data
-    public class KakaoAccount {
+    public static class KakaoAccount {
+        private Boolean profile_nickname_needs_agreement;
+        private Profile profile;
+        private Boolean has_email;
+        private Boolean email_needs_agreement;
+        private Boolean is_email_valid;
+        private Boolean is_email_verified;
+        private String email;
+        private Boolean has_birthday;
+        private Boolean birthday_needs_agreement;
+        private String birthday;
+        private String birthday_type;
+        private Boolean has_gender;
+        private Boolean gender_needs_agreement;
+        private String gender;
+        private Map<String, Object> additional_properties = new LinkedHashMap<>();
 
-
+        @Data
+        public static class Profile {
+            private String nickname;
+            private Map<String, Object> additional_properties = new LinkedHashMap<>();
+        }
     }
-
-
 }
